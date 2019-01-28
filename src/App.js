78be677299e8777eed895ee8827d29c5fe111b83
isaink <inks.minds@gmail.com>
2019-01-28 14:27:29 -0500

    clickerGame

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { MainArea } from '/components/MainArea'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainArea />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MainArea.js
@@ -0,0 +1,15 @@
+class Main extends Components {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <>
+             Knowledge Clicker
+            </>
+        )
+    }
+}
